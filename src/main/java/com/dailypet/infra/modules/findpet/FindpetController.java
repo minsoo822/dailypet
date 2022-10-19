1e62b60c170b27f108b989e8b6d00b619c539c05
minsoo822 <minsoo822@naver.com>
2022-10-19 14:22:39 +0900

    Merge branch 'main' of https://github.com/minsoo822/dailypet.git into main

--- src/main/java/com/dailypet/infra/modules/findpet/FindpetController.java
+++ src/main/java/com/dailypet/infra/modules/findpet/FindpetController.java
@@ -1,5 +1,16 @@
 package com.dailypet.infra.modules.findpet;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/find/")
 public class FindpetController {
+	
+	public String findpetList() throws Exception {
+		return "infra/findpet/user/FindpetList";
+	}
+	
+	
 
 }

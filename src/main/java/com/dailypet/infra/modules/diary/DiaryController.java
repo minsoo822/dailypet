298eccb5877d6218879b78f3ff56bd2e13875647
cuj0405@naver.com <cuj0405@naver.com>
2022-11-08 17:05:36 +0900

    diary 아직도 수정중 화남

--- src/main/java/com/dailypet/infra/modules/diary/DiaryController.java
+++ src/main/java/com/dailypet/infra/modules/diary/DiaryController.java
@@ -43,7 +43,7 @@ public class DiaryController {
 		dto.setIfmmSeq((String)httpSession.getAttribute("sessSeq"));  
 		service.insertDiary(dto);
 		
-		return "redirect:/diary/diaryForm";
+		return "redirect:/diary/diaryList";
 	}
 	
 	

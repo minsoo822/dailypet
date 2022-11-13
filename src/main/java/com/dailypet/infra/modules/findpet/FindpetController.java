3a1eef7e97812720d7c5a8d2446caf63da90da3d
minsoo822@naver.com <minsoo822@naver.com>
2022-11-13 18:11:39 +0900

    findpetController

---
+++ src/main/java/com/dailypet/infra/modules/findpet/FindpetController.java
@@ -0,0 +1,29 @@
+package com.dailypet.infra.modules.findpet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/findpet/")
+public class FindpetController {
+	
+	@RequestMapping(value = "findpetList")
+	public String findpetList() throws Exception {
+		return "infra/findpet/findpetList";
+	}
+	@RequestMapping(value = "findpetView")
+	public String findpetView() throws Exception {
+		return "infra/findpet/findpetView";
+	}
+	@RequestMapping(value = "findpetMod")
+	public String findpetMod() throws Exception {
+		return "infra/findpet/findpetMod";
+	}
+	@RequestMapping(value = "findpetSearchForm")
+	public String findpetSearchForm() throws Exception {
+		return "infra/findpet/findpetSearchForm";
+	}
+	
+	
+
+}

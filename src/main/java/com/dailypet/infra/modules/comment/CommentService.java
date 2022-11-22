3d8a193f5d705b05f11f6d85554a582111bf62c0
cuj0405@naver.com <cuj0405@naver.com>
2022-11-22 15:31:28 +0900

    게시물 좋아요 수정중

--- src/main/java/com/dailypet/infra/modules/comment/CommentService.java
+++ src/main/java/com/dailypet/infra/modules/comment/CommentService.java
@@ -2,7 +2,7 @@ package com.dailypet.infra.modules.comment;
 
 public interface CommentService {
 
-//	댓글
+	// 댓글
 	public int commentInst(Comment dto) throws Exception;
 	public Comment commentOne(Comment dto) throws Exception;
 	

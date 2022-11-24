cddf3f7cfad8666a781a1c4c5af7f54adbdeccf0
jjjy879 <jjjy879@naver.com>
2022-11-24 09:14:30 +0900

    오류 수정

--- src/main/java/com/dailypet/infra/modules/reservation/ReservationController.java
+++ src/main/java/com/dailypet/infra/modules/reservation/ReservationController.java
@@ -2,16 +2,18 @@ package com.dailypet.infra.modules.reservation;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.dailypet.infra.modules.member.MemberVo;
-
 @Controller
 @RequestMapping(value = "/reservation/")
 public class ReservationController {
+	
+	@Autowired
+	ReservationServiceImpl service;
 	
 	@RequestMapping(value = "reservationPage")
 	public String reservationPage(ReservationVo vo, Model model, RedirectAttributes redirectAttributes, HttpSession httpSession) throws Exception {
@@ -42,12 +44,12 @@ public class ReservationController {
 	}
 	
 	@RequestMapping(value = "searchingPlace")
-	public String searchingPlace(MemberVo vo, Model model, HttpSession httpSession) throws Exception {
+	public String searchingPlace(Reservation dto, ReservationVo vo, Model model, HttpSession httpSession) throws Exception {
 		
 		String ifmmSeq = (String) httpSession.getAttribute("sessSeq");
 		vo.setIfmmSeq(ifmmSeq);
 		
-		Reservation selectDefault = service.selectDefault(vo);
+		int selectDefault = service.selectDefault(dto);
 		model.addAttribute("user", selectDefault);
 		
 		return "infra/reservation/user/searchingPlace";
